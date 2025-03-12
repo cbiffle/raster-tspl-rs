ee309cbd86bc95341eaf14370b030a2840eee41e
Cliff L. Biffle <code@cliffle.com>
2025-03-12 09:50:10 -0700

    Simplify sys crate pragmas.

--- cups-filter-sys/src/lib.rs
+++ cups-filter-sys/src/lib.rs
@@ -1,8 +1,6 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(unsafe_op_in_unsafe_fn)] // TODO bindgen bug
-#![allow(improper_ctypes)]
+#![allow(nonstandard_style)] // hey man, I didn't pick these names
+#![allow(unsafe_op_in_unsafe_fn)] // TODO bindgen bug #3147
+#![allow(improper_ctypes)] // TODO this is u128 standing in for long double
 
 #![allow(clippy::all)]
 
