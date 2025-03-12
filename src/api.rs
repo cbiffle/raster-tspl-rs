6017b3b3d2dd373c15763d6ea14844ec06cc37c3
Cliff L. Biffle <code@cliffle.com>
2025-03-12 06:57:51 -0700

    Provide basic CUPS API wrappers to manage pointers.

---
+++ src/api.rs
@@ -0,0 +1,241 @@
+use std::{error::Error, ffi::{c_int, CStr}, fs::File, mem::MaybeUninit, ops::{Deref, DerefMut}, os::fd::{AsRawFd, IntoRawFd}, path::Path, ptr::{null_mut, NonNull}, str::FromStr};
+
+use cups_filter_sys::{cupsFreeOptions, cupsMarkOptions, cupsParseOptions, cupsRasterClose, cupsRasterOpen, cupsRasterReadHeader2, cupsRasterReadPixels, cups_mode_e_CUPS_RASTER_READ, cups_option_t, cups_page_header2_t, cups_raster_t, ppdClose, ppdErrorString, ppdFindMarkedChoice, ppdLastError, ppdMarkDefaults, ppdOpenFd, ppd_choice_t, ppd_file_t};
+
+pub struct PpdFile(NonNull<ppd_file_t>);
+
+impl PpdFile {
+    pub fn open_file(path: impl AsRef<Path>) -> Result<Self, std::io::Error> {
+        let f = File::open(path)?;
+        let p = unsafe {
+            ppdOpenFd(f.into_raw_fd())
+        };
+        if let Some(p) = NonNull::new(p) {
+            Ok(Self(p))
+        } else {
+            let mut linenum = 0;
+            let status = unsafe {
+                ppdLastError(&mut linenum)
+            };
+            let status_str = unsafe {
+                CStr::from_ptr(ppdErrorString(status))
+            };
+            let status_str = status_str.to_string_lossy();
+
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("PPD load failed: line {linenum}: {status_str}"),
+            ))
+        }
+    }
+
+    pub fn raw(&self) -> &ppd_file_t {
+        unsafe {
+            self.0.as_ref()
+        }
+    }
+
+    pub fn raw_mut(&mut self) -> &mut ppd_file_t {
+        unsafe {
+            self.0.as_mut()
+        }
+    }
+
+    pub fn mark_defaults(&mut self) {
+        unsafe {
+            ppdMarkDefaults(self.raw_mut())
+        }
+    }
+
+    pub fn mark_options(&mut self, options: &mut Options) {
+        unsafe {
+            cupsMarkOptions(self.raw_mut(), options.len() as c_int, options.as_mut_ptr());
+        }
+    }
+
+    pub fn find_marked_choice<'s>(&'s mut self, keyword: &CStr) -> Option<PpdChoice<'s>> {
+        let choice = unsafe {
+            ppdFindMarkedChoice(self.raw_mut(), keyword.as_ptr())
+        };
+        unsafe {
+            choice.as_ref().map(PpdChoice)
+        }
+    }
+
+    pub fn parse_default_marked_choice<T>(
+        &mut self,
+        keyword: &CStr,
+    ) -> Result<Option<T>, Box<dyn Error>>
+        where T: FromStr,
+              T::Err: Error + 'static,
+    {
+        self.parse_optional_marked_choice(keyword, c"Default")
+    }
+
+    pub fn parse_optional_marked_choice<T>(
+        &mut self,
+        keyword: &CStr,
+        default: &CStr,
+    ) -> Result<Option<T>, Box<dyn Error>>
+        where T: FromStr,
+              T::Err: Error + 'static,
+    {
+        if let Some(choice) = self.find_marked_choice(keyword) {
+            choice.parse_if_not(default)
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Drop for PpdFile {
+    fn drop(&mut self) {
+        unsafe {
+            ppdClose(self.0.as_ptr());
+        }
+    }
+}
+
+/*
+impl Deref for PpdFile {
+    type Target = ppd_file_t;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            self.0.as_ref()
+        }
+    }
+}
+
+impl DerefMut for PpdFile {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            self.0.as_mut()
+        }
+    }
+}
+*/
+
+pub struct PpdChoice<'a>(&'a ppd_choice_t);
+
+impl PpdChoice<'_> {
+    pub fn choice(&self) -> &CStr {
+        unsafe {
+            CStr::from_ptr(self.0.choice.as_ptr())
+        }
+    }
+
+    pub fn parse_if_not<T: FromStr>(&self, default: &CStr) -> Result<Option<T>, Box<dyn Error>>
+        where T::Err: Error + 'static,
+    {
+        if self.choice() == default {
+            Ok(None)
+        } else {
+            Ok(Some(default.to_str()?.parse()?))
+        }
+    }
+}
+
+
+pub struct Options(NonNull<cups_option_t>, usize);
+
+impl Options {
+    pub fn parse(arg: &CStr) -> Self {
+        let mut options: *mut cups_option_t = null_mut();
+        let num_options = unsafe {
+            cupsParseOptions(arg.as_ptr(), 0, &mut options)
+        };
+
+        if let Some(nn) = NonNull::new(options) {
+            Self(nn, usize::try_from(num_options).unwrap())
+        } else {
+            Self(NonNull::dangling(), 0)
+        }
+    }
+}
+
+impl Drop for Options {
+    fn drop(&mut self) {
+        if self.1 > 0 {
+            unsafe {
+                cupsFreeOptions(self.1 as c_int, self.0.as_ptr());
+            }
+        }
+    }
+}
+
+impl Deref for Options {
+    type Target = [cups_option_t];
+
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            std::slice::from_raw_parts(self.0.as_ptr(), self.1)
+        }
+    }
+}
+
+impl DerefMut for Options {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            std::slice::from_raw_parts_mut(self.0.as_ptr(), self.1)
+        }
+    }
+}
+
+pub struct Raster {
+    _handle: Box<dyn AsRawFd>,
+    raw: NonNull<cups_raster_t>,
+}
+
+impl Raster {
+    pub fn open_file(path: impl AsRef<Path>) -> Result<Self, std::io::Error> {
+        Self::new(Box::new(File::open(path)?))
+    }
+
+    pub fn stdin() -> Result<Self, std::io::Error> {
+        let stdin = std::io::stdin();
+        Self::new(Box::new(stdin.lock()))
+    }
+
+    pub fn new(source: Box<dyn AsRawFd>) -> Result<Self, std::io::Error> {
+        let ras = unsafe {
+            cupsRasterOpen(source.as_raw_fd(), cups_mode_e_CUPS_RASTER_READ)
+        };
+
+        let raw = NonNull::new(ras).ok_or_else(|| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "couldn't open raster stream",
+            )
+        })?;
+        Ok(Self { _handle: source, raw })
+    }
+
+    pub fn read_header2(&mut self) -> Result<cups_page_header2_t, std::io::Error> {
+        let mut header: MaybeUninit<cups_page_header2_t> = MaybeUninit::uninit();
+        let r = unsafe {
+            cupsRasterReadHeader2(self.raw.as_ptr(), header.as_mut_ptr())
+        };
+        if r == 0 {
+            // TODO: this may not have been an OS error!
+            return Err(std::io::Error::last_os_error());
+        }
+
+        Ok(unsafe { header.assume_init() })
+    }
+
+    pub fn read_pixels(&mut self, buffer: &mut [u8]) -> usize {
+        let r = unsafe {
+            cupsRasterReadPixels(self.raw.as_ptr(), buffer.as_mut_ptr(), buffer.len().try_into().unwrap())
+        };
+        r as usize
+    }
+}
+
+impl Drop for Raster {
+    fn drop(&mut self) {
+        unsafe {
+            cupsRasterClose(self.raw.as_ptr())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::{error::Error, ffi::{c_int, CStr, CString}, fs::File, mem::MaybeUninit, os::{fd::AsRawFd, unix::ffi::OsStrExt}, process::exit, ptr::null_mut, str::FromStr, sync::{atomic::{AtomicBool, Ordering}, Arc}};
+mod api;
+
+use std::{error::Error, ffi::{c_int, CString}, os::unix::ffi::OsStrExt, process::exit, sync::{atomic::{AtomicBool, Ordering}, Arc}};
 use std::io::Write;
 
-use cups_filter_sys::{cupsFreeOptions, cupsMarkOptions, cupsParseOptions, cupsRasterClose, cupsRasterOpen, cupsRasterReadHeader2, cupsRasterReadPixels, cups_mode_e_CUPS_RASTER_READ, cups_option_t, cups_page_header2_t, ppdErrorString, ppdFindMarkedChoice, ppdLastError, ppdMarkDefaults, ppdOpenFile, ppd_choice_t, ppd_file_t};
+use api::{Options, PpdFile, Raster};
+use cups_filter_sys::cups_page_header2_t;
 
 const WHITE_THRESHOLD: u8 = 128;
 
@@ -53,17 +56,10 @@ fn error_main() -> Result<(), Box<dyn Error>> {
 
     // Open the page stream
 
-    // Local for keeping the file alive until we're done with it.
-    let input: Box<dyn AsRawFd>;
-
-    if let Some(filename) = args.get(6) {
-        input = Box::new(File::open(filename)?);
+    let mut ras = if let Some(filename) = args.get(6) {
+        Raster::open_file(filename)?
     } else {
-        let stdin = std::io::stdin();
-        input = Box::new(stdin.lock());
-    };
-    let ras = unsafe {
-        cupsRasterOpen(input.as_raw_fd(), cups_mode_e_CUPS_RASTER_READ)
+        Raster::stdin()?
     };
 
     // Register a signal handler to let us know if we get cancelled.
@@ -71,43 +67,22 @@ fn error_main() -> Result<(), Box<dyn Error>> {
     signal_hook::flag::register(signal_hook::consts::SIGTERM, cancelled.clone())?;
 
     // Open the PPD file and apply options
-    let mut options: *mut cups_option_t = null_mut();
-    let args_c = CString::new(args[5].as_bytes())?;
-    let num_options = unsafe {
-        cupsParseOptions(args_c.as_ptr(), 0, &mut options)
+    let mut options = {
+        let args_c = CString::new(args[5].as_bytes())?;
+        Options::parse(&args_c)
     };
-    let ppd = {
-        let ppd_path = std::env::var_os("PPD").ok_or("missing PPD env var")?;
-        let ppd_path_c = CString::new(ppd_path.as_bytes())?;
-        unsafe {
-            ppdOpenFile(ppd_path_c.as_ptr())
-        }
-    };
-    if ppd.is_null() {
-        eprintln!("ERROR: the PPD file could not be opened");
-        let mut linenum = 0;
-        let status = unsafe { ppdLastError(&mut linenum) };
-        let status_str = unsafe { CStr::from_ptr(ppdErrorString(status)) }.to_string_lossy();
-        eprintln!("DEBUG: {status_str} on line {linenum}.");
-        return Err("the PPD file could not be opened".into());
-    }
+    let mut ppd = PpdFile::open_file(std::env::var("PPD")?)?;
 
-    unsafe {
-        ppdMarkDefaults(ppd);
-        cupsMarkOptions(ppd, num_options, options);
-    }
+    PpdFile::mark_defaults(&mut ppd);
+    PpdFile::mark_options(&mut ppd, &mut options);
 
-    setup(ppd)?;
+    setup(&ppd)?;
 
     let mut page = 0;
     loop {
-        let mut header: MaybeUninit<cups_page_header2_t> = MaybeUninit::uninit();
-        let r = unsafe {
-            cupsRasterReadHeader2(ras, header.as_mut_ptr())
-        };
-        if r == 0 {
+        let Ok(header) = ras.read_header2() else {
             break;
-        }
+        };
 
         if cancelled.load(Ordering::Relaxed) {
             break;
@@ -115,9 +90,7 @@ fn error_main() -> Result<(), Box<dyn Error>> {
 
         page += 1;
 
-        let header = unsafe { header.assume_init() };
-
-        start_page(ppd, &header)?;
+        start_page(&mut ppd, &header)?;
 
         let mut buffer = vec![0; header.cupsBytesPerLine as usize];
 
@@ -133,29 +106,21 @@ fn error_main() -> Result<(), Box<dyn Error>> {
             }
 
             // Read a line of graphics
-            let r = unsafe {
-                cupsRasterReadPixels(ras, buffer.as_mut_ptr(), header.cupsBytesPerLine)
-            };
-            if r < 1 {
+            let r = ras.read_pixels(&mut buffer);
+            if r == 0 {
                 break;
             }
 
-            output_line(ppd, &header, y, &buffer)?;
+            output_line(&ppd, &header, y, &buffer)?;
         }
 
         eprintln!("INFO: finished page {page}");
 
-        end_page(ppd, &header)?;
+        end_page(&ppd, &header)?;
 
         if cancelled.load(Ordering::Relaxed) {
-            break; // TODO does this mean to only break the inner loop?
+            break;
         }
-    }
-
-    // Close the raster stream
-    unsafe {
-        cupsRasterClose(ras);
-        cupsFreeOptions(num_options, options);
     }
 
     if page == 0 {
@@ -167,9 +132,8 @@ fn error_main() -> Result<(), Box<dyn Error>> {
 
 const BEEPRT: c_int = 37155;
 
-fn setup(ppd: *mut ppd_file_t) -> Result<(), Box<dyn Error>> {
-    let ppd = unsafe { &*ppd };
-    match ppd.model_number {
+fn setup(ppd: &PpdFile) -> Result<(), Box<dyn Error>> {
+    match ppd.raw().model_number {
         BEEPRT => {
             // nothing to do here
             Ok(())
@@ -184,9 +148,8 @@ enum MediaTracking {
     Continuous,
 }
 
-fn start_page(ppd: *mut ppd_file_t, header: &cups_page_header2_t) -> Result<(), Box<dyn Error>> {
-    let ppd = unsafe { &mut *ppd };
-    match ppd.model_number {
+fn start_page(ppd: &mut PpdFile, header: &cups_page_header2_t) -> Result<(), Box<dyn Error>> {
+    match ppd.raw().model_number {
         BEEPRT => {
             let dots_per_mm_x = (10 * header.HWResolution[0]).div_ceil(254);
             let dots_per_mm_y = (10 * header.HWResolution[1]).div_ceil(254);
@@ -196,15 +159,15 @@ fn start_page(ppd: *mut ppd_file_t, header: &cups_page_header2_t) -> Result<(), 
 
             out!("SIZE {width_mm} mm,{height_mm} mm");
 
-            let reference_x = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"AdjustHoriaontal", c"Default")?
+            let reference_x = ppd.parse_default_marked_choice(c"AdjustHoriaontal")?
                 .unwrap_or(0);
-            let reference_y = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"AdjustVertical", c"Default")?
+            let reference_y = ppd.parse_default_marked_choice(c"AdjustVertical")?
                 .unwrap_or(0);
-            let rotate = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"Rotate", c"Default")?
+            let rotate = ppd.parse_default_marked_choice(c"Rotate")?
                 .unwrap_or(0);
 
             let mut media_tracking = MediaTracking::Gap;
-            if let Some(choice) = ppd_find_marked_choice(ppd, c"zeMediaTracking") {
+            if let Some(choice) = ppd.find_marked_choice(c"zeMediaTracking") {
                 if choice.choice() == c"BLine" {
                     media_tracking = MediaTracking::BLine;
                 } else if choice.choice() == c"Continuous" {
@@ -212,17 +175,17 @@ fn start_page(ppd: *mut ppd_file_t, header: &cups_page_header2_t) -> Result<(), 
                 }
             }
 
-            let gap_mark_height = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"GapOrMarkHeight", c"Default")?
+            let gap_mark_height = ppd.parse_default_marked_choice(c"GapOrMarkHeight")?
                 .unwrap_or(3);
-            let gap_mark_offset = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"GapOrMarkOffset", c"Default")?
+            let gap_mark_offset = ppd.parse_default_marked_choice(c"GapOrMarkOffset")?
                 .unwrap_or(0);
-            let feed_offset = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"FeedOffset", c"Default")?
+            let feed_offset = ppd.parse_default_marked_choice(c"FeedOffset")?
                 .unwrap_or(0);
-            let darkness = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"Darkness", c"Default")?
+            let darkness = ppd.parse_default_marked_choice(c"Darkness")?
                 .unwrap_or(8);
-            let speed = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"zePrintRate", c"Default")?
+            let speed = ppd.parse_default_marked_choice(c"zePrintRate")?
                 .unwrap_or(4);
-            let autodotted = ppd_find_marked_choice_and_parse_if_not::<i32>(ppd, c"Autodotted", c"Default")?
+            let autodotted = ppd.parse_default_marked_choice(c"Autodotted")?
                 .unwrap_or(0);
 
             out!("REFERENCE {},{}",
@@ -258,58 +221,11 @@ fn start_page(ppd: *mut ppd_file_t, header: &cups_page_header2_t) -> Result<(), 
         }
         x => unimplemented!("model number {x}"),
     }
-    // TODO original allocated a global buffer here
-    // TODO original set Feed=0 here
     Ok(())
 }
 
-fn ppd_find_marked_choice<'p>(ppd: &'p mut ppd_file_t, keyword: &CStr) -> Option<PpdChoice<'p>> {
-    let choice = unsafe {
-        ppdFindMarkedChoice(ppd, keyword.as_ptr())
-    };
-    unsafe {
-        choice.as_ref().map(PpdChoice)
-    }
-}
-
-fn ppd_find_marked_choice_and_parse_if_not<T>(
-    ppd: &mut ppd_file_t,
-    keyword: &CStr,
-    default: &CStr,
-) -> Result<Option<T>, Box<dyn Error>>
-where T: FromStr,
-      T::Err: Error + 'static,
-{
-    if let Some(choice) = ppd_find_marked_choice(ppd, keyword) {
-        choice.parse_if_not(default)
-    } else {
-        Ok(None)
-    }
-}
-
-struct PpdChoice<'a>(&'a ppd_choice_t);
-
-impl PpdChoice<'_> {
-    fn choice(&self) -> &CStr {
-        unsafe {
-            CStr::from_ptr(self.0.choice.as_ptr())
-        }
-    }
-
-    fn parse_if_not<T: FromStr>(&self, default: &CStr) -> Result<Option<T>, Box<dyn Error>>
-        where T::Err: Error + 'static,
-    {
-        if self.choice() == default {
-            Ok(None)
-        } else {
-            Ok(Some(default.to_str()?.parse()?))
-        }
-    }
-}
-
-fn output_line(ppd: *mut ppd_file_t, _header: &cups_page_header2_t, _y: u32, buffer: &[u8]) -> Result<(), Box<dyn Error>> {
-    let ppd = unsafe { &*ppd };
-    match ppd.model_number {
+fn output_line(ppd: &PpdFile, _header: &cups_page_header2_t, _y: u32, buffer: &[u8]) -> Result<(), Box<dyn Error>> {
+    match ppd.raw().model_number {
         BEEPRT => {
             // Convert 8-bit grayscale to 1-bit black and white
             for chunk in buffer.chunks(8) {
@@ -329,11 +245,10 @@ fn output_line(ppd: *mut ppd_file_t, _header: &cups_page_header2_t, _y: u32, buf
     Ok(())
 }
 
-fn end_page(ppd: *mut ppd_file_t, _header: &cups_page_header2_t) -> Result<(), Box<dyn Error>> {
-    let ppd = unsafe { &*ppd };
-    match ppd.model_number {
+fn end_page(ppd: &PpdFile, _header: &cups_page_header2_t) -> Result<(), Box<dyn Error>> {
+    match ppd.raw().model_number {
         BEEPRT => {
-            out!("\nPRINT 1,1");
+            out!("\r\nPRINT 1,1");
         }
         x => unimplemented!("model number {x}"),
     }
